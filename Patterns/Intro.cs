0b8cae437586c35eb6e46e39e02309611db3278a
Tiller <tylere431@gmail.com>
2022-12-31 01:55:42 -0600

    finish the build.yml

---
+++ Patterns/Intro.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using LeagueBot;
+using LeagueBot.Patterns;
+using LeagueBot.Game.Enums;
+using LeagueBot.Game.Misc;
+
+namespace LeagueBot
+{
+    public class Intro : PatternScript
+    {
+        
+    }
+
+}
